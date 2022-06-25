88040eaf9c68a9799307c82a28320c54a19b607c
Shubham <shubhamsikarwar43@gmail.com>
2022-06-25 15:08:24 +0530

    adding todo's

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from 'react'
+import './App.css'
 
-function App() {
+export default function App() {
+
+  const [todo, setTodo] = useState([])
+  const [post, setPost] = useState({ text: '', status: false })
+  function addTodo() {
+    const newTodo = [{ text: post, status: false }, ...todo]
+    setTodo(newTodo)
+    setPost({text: ""})
+  }
+  function deleteTodo(i) {
+    const newTodo = [...todo]
+    newTodo.splice(i, 1)
+    setTodo(newTodo)
+  }
+  function changeStatus(i) {
+    const newTodo = [...todo]
+    newTodo[i].status = !newTodo[i].status
+    setTodo(newTodo)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <input
+        onChange={(e) => setPost(e.target.value)}
+        value={post.text}
+        placeholder='add your task here'
+      />
+      <button onClick={() => addTodo()}>Add</button>
+      {todo &&
+        todo.map((tod, i) => {
+          return (
+            <div className={`card ${tod.status ? 'compcard' : ''}`} key={i}>
+              <div className='textarea'>
+                <p>{tod.text}</p>
+              </div>
+              <button onClick={() => deleteTodo(i)}>Delete</button>
+              <button onClick={() => changeStatus(i)}>
+                {tod.status ? 'Completed' : 'Incomplete'}
+              </button>
+            </div>
+          )
+        })}
     </div>
-  );
+  )
 }
-
-export default App;
